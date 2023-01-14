199eef9d5d0d5bb8a9f8bb2c7189ec696c523ab2
qwoti <qwoticreator@gmail.com>
2023-01-14 23:47:24 +0700

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Данил Белов on 14.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
